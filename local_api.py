d0d000189b1f6bcaefabb892405e1ccef9193c5e
root <root@JacobG-HPi7Blue.rdskck.net>
2024-04-09 11:48:06 -0500

    Updated root

--- local_api.py
+++ local_api.py
@@ -1,17 +1,17 @@
 import json
-
 import requests
 
-# TODO: send a GET using the URL http://127.0.0.1:8000
-r = # Your code here
+# Send a GET request
+url_get = "http://127.0.0.1:8000"
+r_get = requests.get(url_get)
 
-# TODO: print the status code
-# print()
-# TODO: print the welcome message
-# print()
+# Print the status code
+print("GET Request Status Code:", r_get.status_code)
 
+# Print the welcome message
+print("Welcome Message from GET Request:", r_get.text)
 
-
+# Data for the POST request
 data = {
     "age": 37,
     "workclass": "Private",
@@ -29,10 +29,13 @@ data = {
     "native-country": "United-States",
 }
 
-# TODO: send a POST using the data above
-r = # Your code here
+# Send a POST request
+url_post = "http://127.0.0.1:8000/data/"
+r_post = requests.post(url_post, json=data)
 
-# TODO: print the status code
-# print()
-# TODO: print the result
-# print()
+# Print the status code
+print("POST Request Status Code:", r_post.status_code)
+
+# Print the result
+print("Result from POST Request:", r_post.json()["result"])
+
--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,42 @@
 import pytest
-# TODO: add necessary import
+import numpy as np
+from ml.model import train_model, compute_model_metrics
+from sklearn.ensemble import RandomForestClassifier
 
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
-    """
-    # add description for the first test
-    """
-    # Your code here
-    pass
+def test_train_model_returns_model():
+    # Sample data
+    X_train = np.array([[1, 2], [3, 4], [5, 6]])
+    y_train = np.array([0, 1, 0])
+    # Train the model
+    model = train_model(X_train, y_train)
+    # Assert the type of the trained model
+    assert isinstance(model, RandomForestClassifier)
+
+def test_compute_model_metrics_expected_values():
+    # Sample true and predicted labels
+    y_true = np.array([0, 1, 0, 1, 0, 1])
+    y_pred = np.array([0, 1, 1, 1, 0, 0])
+    # Compute model metrics
+    precision, recall, f1 = compute_model_metrics(y_true, y_pred)
+    # Assert expected values
+    assert round(precision, 4) == 0.6667
+    assert round(recall, 4) == 0.6667
+    assert round(f1, 4) == 0.6667
+
+def test_data_types():
+    # Sample data
+    X_train = np.array([[1, 2], [3, 4], [5, 6]])
+    y_train = np.array([0, 1, 0])
+    X_test = np.array([[7, 8], [9, 10]])
+    y_test = np.array([1, 0])
+    # Assert data types
+    assert isinstance(X_train, np.ndarray)
+    assert isinstance(y_train, np.ndarray)
+    assert isinstance(X_test, np.ndarray)
+    assert isinstance(y_test, np.ndarray)
+    assert X_train.dtype == np.int64
+    assert y_train.dtype == np.int64
+    assert X_test.dtype == np.int64
+    assert y_test.dtype == np.int64
 
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
-    """
-    # add description for the second test
-    """
-    # Your code here
-    pass
-
-
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
-    """
-    # add description for the third test
-    """
-    # Your code here
-    pass
